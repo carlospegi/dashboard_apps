73b86e3f8dca6f1639f56f45091abd42ea52f4ce
carlos <carlos.pegi.01@gmail.com>
2022-10-23 09:02:30 -0700

    Sidebar , funtion size window,//   sidebar true . false

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -6,7 +6,7 @@ import { SiShopware } from 'react-icons/si'
 import { MdOutlineCancel } from 'react-icons/md'
 import { TooltipComponent } from '@syncfusion/ej2-react-popups'
 import { links } from '../data/dummy';
-import {useStateContext} from '../context/ContextProvider'
+import { useStateContext } from '../context/ContextProvider'
 
 const activeLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg  text-white  text-md m-2';
 const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md text-gray-700 dark:text-gray-200 dark:hover:text-black hover:bg-light-gray m-2';
@@ -15,18 +15,24 @@ const normalLink = 'flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-md 
 
 const Sidebar = () => {
 
-  const {activeMenu, setActiveMenu } = useStateContext()
+  const { activeMenu, setActiveMenu, screenSize } = useStateContext()
+
+const handleCloseSideBar = () =>{
+  if(activeMenu && screenSize <= 900){
+    setActiveMenu(false)
+  }
+}
+
   return (
-    <div className='ml-3 h-screen md:overflow-hidden overflow-auto
-    md:hover:overflow-auto pb-10'>
-      {activeMenu && <>
+    <div className='ml-3 h-screen md:overflow-hidden overflow-auto md:hover:overflow-auto pb-10'>
+      {activeMenu && (<>
         <div className='flex justify-between items-center' >
-          <Link to={'/'} onClick={() => setActiveMenu(false) } className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900" >
+          <Link to={'/'} onClick={ handleCloseSideBar} className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900" >
             <SiShopware /> <span>Shoppy</span>
           </Link>
-          <TooltipComponent content="menu" position='BottonCenter'>
-            <button tipe="button" onClick={() => setActiveMenu(!activeMenu)} // TODO: check pass prev
-             className="text-xl rounded-full hover:bg-light-gray p-3 mt-4 block md:hidden" >
+          <TooltipComponent content="menu" position="BottomCenter">
+            <button tipe="button" onClick={() => setActiveMenu(!activeMenu)} // click logo
+              className="text-xl rounded-full hover:bg-light-gray p-3 mt-4 block md:hidden" >
               <MdOutlineCancel />
             </button>
           </TooltipComponent>
@@ -41,7 +47,7 @@ const Sidebar = () => {
               {item.links.map(link => (
                 <NavLink to={`/${link.name}`}
                   key={link.name}
-                  onClick={() => { }}
+                  onClick={handleCloseSideBar }
                   className={({ isActive }) => isActive ? activeLink : normalLink}
                 >
                   {link.icon}
@@ -51,7 +57,7 @@ const Sidebar = () => {
             </div>
           ))}
         </div>
-      </>}
+      </>)}
     </div>
   )
 }
--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -2,20 +2,25 @@ const { createContext, useContext, useState } = require("react");
 
 const StateContext = createContext()
 
-const initialState= {
-    chat:false,
-    cart:false,
-    userProfile:false,
-    notification:false
+const initialState = {
+    chat: false,
+    cart: false,
+    userProfile: false,
+    notification: false
 }
 
-export const ContextProvider= ({children})=>{
+export const ContextProvider = ({ children }) => {
     const [activeMenu, setActiveMenu] = useState(true);
+    const [isClicked, setIsClicked] = useState(initialState);
+ const [screenSize, setScreenSize] = useState(undefined);
+    const handleClick = (click) => {
+        setIsClicked({ ...initialState, [click]: true })
+    }
     return (
-        < StateContext.Provider value={{activeMenu, setActiveMenu}} >
-        {children}
+        < StateContext.Provider value={{ activeMenu, setActiveMenu, isClicked, setIsClicked, handleClick, screenSize, setScreenSize }} >
+            {children}
         </StateContext.Provider>
     )
-} 
+}
 
-export const useStateContext = ()=> useContext(StateContext)
+export const useStateContext = () => useContext(StateContext)
