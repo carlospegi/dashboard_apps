090a4d3622b72c55204b7503f285779a224f2ced
carlos <carlos.pegi.01@gmail.com>
2022-10-30 13:47:22 -0700

    data-tables  employees , customers

--- src/pages/Employees.jsx
+++ src/pages/Employees.jsx
@@ -1,11 +1,42 @@
 import React from 'react'
+import { GridComponent, ColumnsDirective, ColumnDirective, Page, Search, Inject, Toolbar } from '@syncfusion/ej2-react-grids';
 
-const Employees = () => {
+import { ordersData, contextMenuItems, ordersGrid, employeesGrid, employeesData } from '../data/dummy';
+import { Header } from '../components';
+
+
+const Employees = () => { //229
   return (
-    <div>
-      
+    <div className='m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl'>
+      <Header
+      category="page"
+      title="Employees" 
+      />
+      <GridComponent
+      dataSource={employeesData}
+      allowPaging // paginaton
+      allowSorting
+      toolbar={['Search']}
+      width="auto"
+      >
+        <ColumnsDirective>
+{
+  employeesGrid.map((item,index)=>(
+    <ColumnDirective 
+    key={index}
+{...item}
+
+    />
+  ))
+}
+        </ColumnsDirective>
+        <Inject
+        services={[ Page, Search, Toolbar]} // pag , sort
+         />
+      </GridComponent>
     </div>
   )
 }
 
 export default Employees
+ 
